e047be4b77972bad640c82f75cbfa0758bb08163
wasabi98 <wasabi10231998@gmail.com>
2022-08-02 12:14:22 +0200

    Agent is moving along given path

--- Assets/Scripts/Agent.cs
+++ Assets/Scripts/Agent.cs
@@ -7,10 +7,11 @@ using UnityEngine.AI;
 
 public class Agent : MonoBehaviour
 {
-    [SerializeField] 
-    private Transform destination;
-
     
+
+    [SerializeField] private List<GameObject> path;
+    private int currentDestination;
+    private bool isfollowing;
     
 
     private NavMeshAgent _navMeshAgent;
@@ -18,22 +19,38 @@ public class Agent : MonoBehaviour
     void Start()
     {
         _navMeshAgent = this.GetComponent<NavMeshAgent>();
-        SetDestination();
+        currentDestination = 0;
+        _navMeshAgent.SetDestination(path[currentDestination].transform.position);
+
     }
 
-    private void SetDestination()
+    private void FollowPath()
     {
-        if (destination != null)
-        {
-            Vector3 target = destination.transform.position;
-            _navMeshAgent.SetDestination(target);
-        }
+            /*Vector3 target = destination.transform.position;
+            _navMeshAgent.SetDestination(target);*/
+            
+
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.x == path[currentDestination].transform.position.x &&
+            transform.position.z == path[currentDestination].transform.position.z)
+        {
+            if (currentDestination < path.Count - 1)
+            {
+                currentDestination++;
+            }
+            else
+            {
+                currentDestination = 0;
+            }
+            _navMeshAgent.SetDestination(path[currentDestination].transform.position);
+        }
     }
 }
 
